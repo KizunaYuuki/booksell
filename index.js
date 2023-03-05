d3c91f69e75284878562c2d2d0461e8f93827908
Kizuna Yuuki <kizunayuuki@Kizunas-iMac.local>
2023-03-05 14:11:43 +0700

    handler address (temp)

---
+++ index.js
@@ -0,0 +1,3 @@
+const obj = [
+  {"Id":"01","Name":"Thành phố Hà Nội","Districts":[{"Id":"001","Name":"Quận Ba Đình","Wards": [{"Id":"00001","Name":"Phường Phúc Xá","Level":"Phường"},{"Id":"00004","Name":"Phường Trúc Bạch","Level":"Phường"}]}]}
+]
