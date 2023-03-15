a38b3c429836b0e19cb82972ebf1680216c5d072
thanhb1906764 <thanhb1906764@student.ctu.edu.vn>
2023-03-15 17:22:08 +0700

    Wed Mar 15 17:21:55 +07 2023

--- index.js
+++
@@ -1,3 +0,0 @@
-const obj = [
-  {"Id":"01","Name":"Thành phố Hà Nội","Districts":[{"Id":"001","Name":"Quận Ba Đình","Wards": [{"Id":"00001","Name":"Phường Phúc Xá","Level":"Phường"},{"Id":"00004","Name":"Phường Trúc Bạch","Level":"Phường"}]}]}
-]
